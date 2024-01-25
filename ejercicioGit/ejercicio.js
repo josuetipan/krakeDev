723408a7b137b77ca2692ecc0f6d533580c2baac
josuetipan <josuetipan101@gmail.com>
2024-01-24 20:17:00 -0500

    el boton modificar

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
     console.log("ingresar saludar");
+    let rec;
+    rec=document.getElementById("saludo");
+    rec.src="./imagenes/c561c3ecf915d8890dd9829b418d9a00.gif" 
 }
