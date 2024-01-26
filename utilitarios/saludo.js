39a47e7c79bde81a8ade52f410a0c4f09e6a0b16
josuetipan <josuetipan101@gmail.com>
2024-01-25 21:54:51 -0500

    funcion mostrarTexto agregado

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -12,6 +12,9 @@ saludar=function(){
     ape=recuperarTexto("txtApellido");
     let edad=recuperarInt("txtEdad");
     let estatura=recuperarFloat("txtEstatura");
+
+    let mensajeBienvenidO ="Bienvenido "+recu+" "+ape;
+    mostarTexto("idResultado",mensajeBienvenidO);
 }
 recuperarInt=function(idComponente){
     let valorCaja = recuperarTexto(idComponente);
@@ -23,4 +26,8 @@ recuperarFloat=function(idComponente){
     let valorCaja = recuperarTexto(idComponente);
     let valorFlotante = parseFloat(valorCaja);
     return valorFlotante
+}
+mostarTexto=function(idComponente,mensaje) {
+    let valorCaja =  document.getElementById(idComponente);
+    valorCaja.innerText=mensaje
 }
