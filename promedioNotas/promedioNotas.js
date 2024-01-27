a6e3098f933237641b94003dde1ca4d58d158c3f
josuetipan <josuetipan101@gmail.com>
2024-01-27 17:58:43 -0500

    calcular promedio

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,9 @@
+calcularPromedioNotas = function(){
+    let num1 = recuperarFlotante("lblNota1");
+    let num2 = recuperarFlotante("lblNota2");
+    let num3 = recuperarFlotante("lblNota3");
+    
+    let promedio = calcularPromedio(num1,num2,num3);
+    promedio=promedio.toFixed(2)
+    cambiarTexto("lblResultado",promedio)
+}
