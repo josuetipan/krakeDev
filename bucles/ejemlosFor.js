58a43e6c38dfd1c780840eb0bf0a5f6c4d691cd1
josuetipan <josuetipan101@gmail.com>
2024-02-04 16:54:51 -0500

    bucles

---
+++ bucles/ejemlosFor.js
@@ -0,0 +1,49 @@
+mostrarNumeros1 = function(){
+    console.log("antes del for");
+    for(let i = 0;i<4;i++){
+        console.log(i);
+    }
+
+    console.log("despues del for");
+}
+nostrarNumeros2 = function(){
+    console.log("antes del for");
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+
+    console.log("despues del for");
+}
+mostrarPares = function(){
+    console.log("antes del for");
+    for(let x=2;x<=10;x+=2){
+        console.log(x);
+    }
+
+    
+    console.log("despues del for");
+}
+mostrarInverso = function(){
+    console.log("antes del for");
+    for(let i=10;i>=0;i--){
+        console.log(i);
+    }
+
+    
+    console.log("despues del for");
+}
+hackearNasaPelis = function(){
+    //hackeando nasa 0%
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log("hackeando nasa "+porcentaje+"%");
+
+    }
+    console.log("la nsa ha sido Hackeada")
+}
+mostaraImpares = function(){
+    console.log("Antes del for");
+    for(let i=1;i<=21;i+=2){
+        console.log(i);
+    }
+    console.log("despues del for");
+}
