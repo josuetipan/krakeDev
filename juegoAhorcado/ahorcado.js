ba2d3a3a6c8d504080435e91961d80c44ceddbd1
josuetipan <josuetipan101@gmail.com>
2024-02-11 20:49:13 -0500

    paso 1

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,1 +1,18 @@
-//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta = "";
+esMayuscula = function(caracter){
+    let mayuscula;
+    mayuscula= caracter.charCodeAt()
+    if(mayuscula>=64 && mayuscula<=90){
+        return true;
+    }else{
+        return false;
+    }
+}
+guardarPalabra = function(){
+    let texto = recuperarTexto("txtSecreta");
+    if(texto.length==5 && esMayuscula(texto) == true){
+        palabraSecreta = texto;
+    }else{
+        alert("DEBE INGRESAR UNA PALABRA DE 5 LETRAS MAYUSCULAS");
+    }
+}
