36129f55dc9ed70b78f54ed974ef20dc7f307d5f
josuetipan <josuetipan101@gmail.com>
2024-02-15 22:27:18 -0500

    arreglos frutas

--- arreglos/ejercicio1.js
+++ arreglos/ejercicio1.js
@@ -12,7 +12,27 @@ recorrerArreglo = function(){
         console.log(notaR);
     }
 }
+generarTabla = function(){
+    let contenidoTabla ="";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla+="<table><tr><td>UNO</td></tr>"+
+    "<tr><td>DOS</td></tr></table>"
+    cmpTabla.innerHTML=contenidoTabla;
+}
 
+mostrarNotas = function(){
+    let cmpTabla = document.getElementById("divTabla");
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let miNOta;
+    for(let i= 0;i<notas.length;i++){
+        miNOta = notas[i];
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=miNOta;
+        contenidoTabla+="</tr></td>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
 probaraAgregar = function(){
     let notaRecuperada;
     notaRecuperada = recuperarInt("txtNota");
@@ -20,6 +40,7 @@ probaraAgregar = function(){
 }
 agregarNota= function(nota){
     notas.push(nota);
+    mostrarNotas();
 }
 
 calcularPromedio = function(){
