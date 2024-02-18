b7ea40e4b496317781dd0f57bb10a9a84bb59cc8
josuetipan <josuetipan101@gmail.com>
2024-02-17 23:20:57 -0500

    roles 3.2

--- roles/roles.js
+++ roles/roles.js
@@ -53,3 +53,81 @@ ejecutarNuevo = function(){
     habilitarComponente("btnGuardar");
     esNUevo = true;
 }
+
+buscarEmpleado = function(cedula){
+    let elementosEmpleado;
+    let elementoEncontrado = null;
+    for(let i = 0;i<empleados.length;i++){
+        elementosEmpleado = empleados[i];
+        if(elementosEmpleado.cedula == cedula){
+            elementoEncontrado = elementosEmpleado;
+            break
+        }
+    }
+    return elementoEncontrado;
+}
+agregarEmpleado = function(empleado){
+    let resultadoEmpleo;
+    resultadoEmpleo = buscarEmpleado(empleado.cedula)
+    if (resultadoEmpleo == null){
+        empleados.push(empleado);
+        return true;
+    }else{
+        alert("ya existe el clienete con la cedula : " + empleado.cedula);
+        return false;
+    }
+}
+
+guardar = function(){
+    let cedula = recuperarTexto("txtCedula");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let sueldo = recuperarFloat("txtSueldo");
+    if(cedula.length == 10){                              // longitud de la cedula
+        let mayuscula;
+        mostrarTexto("lblErrorCedula","");
+        mayuscula = nombre.charCodeAt();
+        if(mayuscula>=65 && mayuscula<=90 && nombre.length>=3){ // nombre letras mayusculas
+            mostrarTexto("lblErrorNombre","");
+            let mayusculaApellido;
+            mayusculaApellido = apellido.charCodeAt()
+            if(mayusculaApellido>=65 && mayusculaApellido<=90){// apellido letras mayusculas
+                mostrarTexto("lblErrorApellido","");
+                if(sueldo>=400 && sueldo<=5000 ){               // sueldo 
+                    mostrarTexto("lblErrorSueldo","");
+                    if(esNUevo == true){
+                        let nuevo = {}
+                        nuevo.cedula = cedula;
+                        nuevo.nombre = nombre;
+                        nuevo.apellido = apellido;
+                        nuevo.sueldo = sueldo;
+                        let agregar = agregarEmpleado(nuevo);
+                        if(agregar == true){
+
+                            mostrarEmpleado();
+                            alert("EMPLEADO SE GUARDO CORRECTAMENTE");
+                        }
+                        
+                        
+                    } 
+
+                }else{
+                    mostrarTexto("lblErrorSueldo","Error debe ingresar digitos, sueldo entre 400 y 5000");
+                }
+
+            }else{
+                mostrarTexto("lblErrorApellido","Error debe ingresar letras mayuscula y minimo 3 caracteres");
+
+            }
+
+        }else{
+            mostrarTexto("lblErrorNombre","Error debe ingresar letras mayuscula y minimo 3 caracteres");
+
+        }
+        
+    }else{
+        mostrarTexto("lblErrorCedula","Error debe ingresar 10 digitos");
+       
+    }
+
+}
